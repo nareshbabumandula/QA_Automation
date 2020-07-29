a8b4f1a308ace85b22b126326ee6ab6f3a85ae15
pradeepaguru <kpradeepa82@gmail.com>
2020-07-29 12:53:22 -0400

    Add New line
    Commit: 07/29/2020

--- IKEA_Automation/src/basics/Demo.java
+++ IKEA_Automation/src/basics/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
 		System.out.println("Selenium Java");
                 System.out.println("1st Git hub session");
 		System.out.println("2nd Git hub session");
+		System.out.println("My second commit");
 		
 
 	}
