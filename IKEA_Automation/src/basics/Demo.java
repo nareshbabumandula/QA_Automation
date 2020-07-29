2fcf6d946b6e0f491014d8faaae64dc47f6eff17
Mandula Naresh Babu <naresh2223@gmail.com>
2020-07-29 23:01:01 +0530

    29-July-2020 : Naresh : Updated print statement

--- IKEA_Automation/src/basics/Demo.java
+++ IKEA_Automation/src/basics/Demo.java
@@ -9,7 +9,7 @@ public class Demo {
 		System.out.println("---------------------------");
 		System.out.println("1st Git hub session");
 		System.out.println("2nd Git hub session");
-		System.out.println("My second commit");
+		System.out.println("My second commit..!");
 		
 
 	}
