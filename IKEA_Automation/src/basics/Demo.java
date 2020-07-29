36cdeea4110a7aa0575b077877409715434a76eb
Mandula Naresh Babu <naresh2223@gmail.com>
2020-07-29 23:13:55 +0530

    Test

--- IKEA_Automation/src/basics/Demo.java
+++ IKEA_Automation/src/basics/Demo.java
@@ -3,12 +3,10 @@ package basics;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Core Java Training..!");
-		System.out.println("---------------------------");
-		System.out.println("Selenium Java");
 		System.out.println("---------------------------");
 		System.out.println("1st Git hub session");
 		System.out.println("2nd Git hub session");
+		System.out.println("My second commit");
 		System.out.println("My second commit..!");
 		
 
