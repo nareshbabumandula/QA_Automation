05a7e2b69d67c37b3b447a1a4b538168f1817845
Mandula Naresh Babu <naresh2223@gmail.com>
2020-07-29 22:37:51 +0530

    Updated Demo.java file

--- IKEA_Automation/src/basics/Demo.java
+++ IKEA_Automation/src/basics/Demo.java
@@ -4,8 +4,10 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Core Java Training..!");
+		System.out.println("---------------------------");
 		System.out.println("Selenium Java");
-                System.out.println("1st Git hub session");
+		System.out.println("---------------------------");
+		System.out.println("1st Git hub session");
 		System.out.println("2nd Git hub session");
 		System.out.println("My second commit");
 		
---
+++ IKEA_Automation/src/basics/FuncInterface.java
@@ -0,0 +1,13 @@
+package basics;
+
+public interface FuncInterface {
+
+	// An abstract function 
+	void abstractFun(int x); 
+
+	// A non-abstract (or default) function 
+	default void normalFun() 
+	{ 
+		System.out.println("Hello"); 
+	} 
+}
