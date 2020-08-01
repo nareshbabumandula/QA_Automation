e26956c66327a1c8dc41c89e6955b16d48f2df69
pradeepaguru <kpradeepa82@gmail.com>
2020-08-01 11:22:57 -0400

    Date: 08/01/2002 - Commited for new print statement from deepa user

--- IKEA_Automation/src/basics/Generic.java
+++ IKEA_Automation/src/basics/Generic.java
@@ -5,6 +5,7 @@ public class Generic{
 	public static void main(String[] args) {
 		System.out.println("Main method of Generic class");
 		System.out.println("August 1st class");
+                System.out.println("----------------------------------------------");
 	}
 
 }
